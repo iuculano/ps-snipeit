4296f15260544cfb6dc555c4a03f7a9971aa1394
iuculano <82924525+iuculano@users.noreply.github.com>
2023-01-16 06:50:38 +0000

    Add missing parameters for status labels

--- Public/New-SnipeITStatusLabel.ps1
+++ Public/New-SnipeITStatusLabel.ps1
@@ -8,6 +8,22 @@
     .PARAMETER Type
     Specifies the status label's type.
 
+    .PARAMETER Notes
+    Specifies notes to attach to the status label.
+
+    .PARAMETER Color
+    Specifies the color of the status label.
+
+    This value should be written as hexidecimal.
+
+    .PARAMETER ShowInNav
+    Specifies whether the status label should show in the left-side nav of the
+    web GUI.
+
+    .PARAMETER DefaultLabel
+    Specifies whether the status label should be brought to the top of the list
+    of available statuses.
+
     .PARAMETER Url
     Specifies the SnipeIT endpoint to which the request is sent.
 
@@ -31,8 +47,23 @@ function New-SnipeITStatusLabel
         [String]$Name,
 
         [APISubmittableAttribute()]
+        [Parameter(Mandatory = $true)]
         [ValidateSet("Deployable", "Pending", "Archived")]
         [String]$Type,
+
+        [APISubmittableAttribute()]
+        [ValidateNotNullOrEmpty()]
+        [String]$Notes,
+
+        [APISubmittableAttribute()]
+        [ValidateNotNullOrEmpty()]
+        [String]$Color,
+
+        [APISubmittableAttribute()]
+        [Bool]$ShowInNav,
+
+        [APISubmittableAttribute()]
+        [Bool]$DefaultLabel,
 
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
