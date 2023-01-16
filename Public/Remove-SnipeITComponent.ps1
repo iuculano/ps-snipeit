6d27885f355b5b973c08b144744a63ce1186545c
iuculano <82924525+iuculano@users.noreply.github.com>
2023-01-16 06:50:38 +0000

    Add missing remove functions

---
+++ Public/Remove-SnipeITComponent.ps1
@@ -0,0 +1,79 @@
+<#
+    .SYNOPSIS
+    Remove one or more Snipe-IT companies.
+
+    .PARAMETER Id
+    Specifies a component id to remove.
+
+    .PARAMETER Url
+    Specifies the SnipeIT endpoint to which the request is sent.
+
+    .PARAMETER APIKey
+    Specifies a SnipeIT API key.
+
+
+    .NOTES
+    Remove-SnipeITComponent.ps1
+    Alex Iuculano, 2023
+#>
+
+function Remove-SnipeITComponent
+{
+    [CmdletBinding(DefaultParameterSetName = "Default",
+                   ConfirmImpact           = "High",
+                   SupportsShouldProcess   = $true)]
+    Param
+    (
+        [Parameter(Position          = 0,
+                   ValueFromPipeline = $true,
+                   ParameterSetName  = "InputObject")]
+        [PSTypeName("axSnipeIT.Company")][Object]$InputObject,
+
+        [Parameter(Position          = 0,
+                   Mandatory         = $true,
+                   ValueFromPipeline = $true,
+                   ParameterSetName  = "Default")]
+        [ValidateRange(0, [Int32]::MaxValue)]
+        [Int32[]]$Id,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Url,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$APIKey
+    )
+
+
+    Process
+    {
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            "Default"
+            {
+                $identity = $Id
+                break
+            }
+
+            "InputObject"
+            {
+                $identity = $InputObject.Id
+                break
+            }
+        }
+
+
+        foreach ($i in $identity)
+        {
+            if (!$PSCmdlet.ShouldProcess($i))
+            {
+                return
+            }
+
+
+            $endpoint = "$Url/api/v1/components/$i"
+            Invoke-SnipeITRestMethod -Method "DELETE" -Url $endpoint -APIKey $APIKey
+        }
+    }
+}
